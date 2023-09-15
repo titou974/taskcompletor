b40a8f65b6d11daabc72fba366bbfcd73e4514ff
Titouan Hirsch <titouanhirsch@gmail.com>
2023-09-15 06:34:42 +0200

    little front corrections

--- components/dropdowndoc.js
+++ components/dropdowndoc.js
@@ -30,7 +30,7 @@ const DropDownDoc = ({ type, setType }) => {
     <>
       <Menu as="div" className="relative block text-left w-full text-md">
         <div>
-          <Menu.Button className="inline-flex w-full justify-between items-center rounded-md border-2 border-white bg-[#011B6B] px-4 py-2 text-white shadow-sm hover:bg-white hover:text-[#011B6B] focus:outline-none focus:ring-2 focus:ring-white transition-all">
+          <Menu.Button className="inline-flex w-full justify-between items-center rounded-md border-2 border-white bg-[#011B6B] px-4 py-2 text-white shadow-sm hover:bg-white/[0.12] hover:text-slate-300 hover:border-white/[0.12] focus:outline-none focus:ring-2 focus:ring-white transition-all">
             {type}
             <ChevronUpIcon
               className="-mr-1 ml-2 h-5 w-5 ui-open:hidden"
