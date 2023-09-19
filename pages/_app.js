9f14bfd662017af4edec74781c7d82c81007a79f
Titouan Hirsch <titouanhirsch@gmail.com>
2023-09-19 08:10:10 +0200

    pages transition

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
 import "../css/tailwind.css";
 import Navbar from "../components/navbar";
+import { motion, AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
 
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
   return (
-      <div className='bg-primary'>
+    <AnimatePresence mode='wait'>
+      <motion.div key={router.route} initial="initialState" animate="animatedState" exit="exitState" transition={{ duration: 0.75 }} variants={{ initialState: { opacity: 0, clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)" }, animatedState: { opacity: 1, clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)" }, exitState: {clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)"}}} className='bg-primary'>
         <Component {...pageProps} />
-      </div>
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
--- utils/motion.js
+++ utils/motion.js
@@ -86,3 +86,4 @@ export const staggerContainer = (staggerChildren, delayChildren) => {
     },
   };
 };
+
