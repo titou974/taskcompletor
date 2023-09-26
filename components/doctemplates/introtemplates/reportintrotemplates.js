c4e557a6c1ed97bee600af40cedc3d63d25ea0a3
Titouan Hirsch <titouanhirsch@gmail.com>
2023-09-27 00:35:20 +0200

    delete caroussel on introduction

--- components/doctemplates/introtemplates/reportintrotemplates.js
+++ components/doctemplates/introtemplates/reportintrotemplates.js
@@ -3,9 +3,6 @@ import PageReportPhoneIntro from "./reportpages/pagereportphoneintro";
 import PageReportIntro from "./reportpages/pagereportintro";
 import PageTitleReportIntro from "./reportpages/pagetitlereportintro";
 
-import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-
 
 
 const ReportIntroTemplate = ({ generatedTitle, generatedSections, length }) => {
@@ -16,7 +13,6 @@ const ReportIntroTemplate = ({ generatedTitle, generatedSections, length }) => {
       {/* Caroussel */}
 
       <div className="sm:hidden relative" >
-        <Carousel infiniteLoop autoPlay interval={5000} showStatus={false} showThumbs={false}>
           <div>
           {
             length >= 1 && (
@@ -24,22 +20,8 @@ const ReportIntroTemplate = ({ generatedTitle, generatedSections, length }) => {
               )
             }
           </div>
-          {
-            length >= 3 && (
-              <PageReportPhoneIntro generatedSections={generatedSections.slice(2, 4)} indexSection={3} id="page-2"/>
-            )
-          }
-          <div>
-          {
-            length >= 5 && (
-              <PageReportPhoneIntro generatedSections={generatedSections.slice(4, 6)} indexSection={5} id="page-3"/>
-            )
-          }
-          </div>
-        </Carousel>
       </div>
-      <div className="hidden sm:block w-1/2 mx-auto">
-        <Carousel infiniteLoop autoPlay interval={5000} showStatus={false} showThumbs={false} >
+      <div className="hidden sm:block w-full mx-auto">
           <div>
             {
               length >= 1 && (
@@ -47,14 +29,6 @@ const ReportIntroTemplate = ({ generatedTitle, generatedSections, length }) => {
               )
             }
           </div>
-          <div>
-            {
-              length >= 4 && (
-                <PageReportIntro generatedSections={generatedSections.slice(3, 6)} indexSection={4} id="page-2" />
-              )
-            }
-          </div>
-        </Carousel>
       </div>
     </div>
   );
